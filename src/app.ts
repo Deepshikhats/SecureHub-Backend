23ae06d3e7e16968205129b7d7f0808818ba2239
Deepshikha <deepshikha.mec@gmail.com>
2024-07-19 00:19:39 +0530

    fix: domain change

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import UserRouter from "./routes/userRoutes"
+import UserRouter from "./routes/userRoutes";
 
 /*******************   CONSTANTS  ************************* */
 const app = express();
 const corsOptions = {
-  origin: 'http://localhost:3000', // Allow only this origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these methods
-  allowedHeaders: 'Content-Type,Authorization', // Allow these headers
+  origin: "https://secure-hub-frontend.vercel.app", // Allow only this origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow these methods
+  allowedHeaders: "Content-Type,Authorization", // Allow these headers
 };
 
 /********************** Middlewares *********************** */
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use('/',UserRouter)
+app.use("/", UserRouter);
 
 /********************* DB CONNECT ************************** */
 
